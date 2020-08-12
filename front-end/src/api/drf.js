f94b4579ff81540c688eb3afb800f8354939845c
김소영 <soyeong.kim.95@gmail.com>
2020-08-13 01:33:40 +0900

    edit logout url

--- front-end/src/api/drf.js
+++ front-end/src/api/drf.js
@@ -6,7 +6,8 @@ export default{
         login: '/admin/login',
         logout: '/admin/logout',
         kakaoLogin: '/signin/kakao',
-        kakaoLogout: 'https://kauth.kakao.com/oauth/logout?client_id=3b520fb7e5e907ebebfde93be5b8a1aa&logout_redirect_uri=http://i3b202.p.ssafy.io:80/',
+        // kakaoLogout: 'https://kauth.kakao.com/oauth/logout?client_id=3b520fb7e5e907ebebfde93be5b8a1aa&logout_redirect_uri=http://i3b202.p.ssafy.io:80/',
+        kakaoLogout: 'https://kauth.kakao.com/oauth/logout?client_id=3b520fb7e5e907ebebfde93be5b8a1aa&logout_redirect_uri=http://localhost:8081/',
         kakaoOff: '/user/kakao/unlink',
         getUserInfo: '/user/getUserInfo',
 
