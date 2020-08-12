017aec36aa5ddb4cb67473b0ffed52b70b52cdfd
김소영 <soyeong.kim.95@gmail.com>
2020-08-12 15:36:21 +0900

    fix

--- front-end/src/api/drf.js
+++ front-end/src/api/drf.js
@@ -1,6 +1,6 @@
 export default{
-    // URL:'http://i3b202.p.ssafy.io:8080',
-    URL:'http://localhost:8080',
+    URL:'http://i3b202.p.ssafy.io:8080',
+    // URL:'http://localhost:8080',
     ROUTES: {
         // auth
         login: '/admin/login',
