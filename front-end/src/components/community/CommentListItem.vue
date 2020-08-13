785cb2eb1209c77bd043125d7de91ddcb27c49b7
김도연 <kdymay93@naver.com>
2020-08-13 19:40:34 +0900

    0813

--- front-end/src/components/community/CommentListItem.vue
+++ front-end/src/components/community/CommentListItem.vue
@@ -8,7 +8,7 @@
       <div class="col-10 pl-2">
         <div class="d-flex flex-wrap justify-content-between">
           <div class="d-flex align-center">
-            <h3 class="font-weight-bold">{{ comment.r_name }}</h3>
+            <h3 class="font-weight-bold">{{ comment.cr_name }}</h3>
             <!-- <pre class="text-secondary ml-3">{{ updateTime }}</pre> -->
           </div>
           <div v-if="isAuth" class="ml-auto">
@@ -18,7 +18,7 @@
       </div>
     </div>
         <div class="ml-10 pl-10">
-          <h1 class=""> {{ comment.r_content }}</h1>
+          <h1 class=""> {{ comment.cr_content }}</h1>
         </div>
     <br>
     <hr>
@@ -46,7 +46,7 @@ export default {
   },
   computed: {
     profileImage(){
-      return SERVER.URL + this.comment.r_profileImg
+      return SERVER.URL + this.comment.cr_profileImg
     },
     updateTime(){
       return this.comment.updated_at.slice(0,10)
@@ -55,7 +55,7 @@ export default {
 
   methods: {
     deleteComment(){
-      const idx = this.comment.r_idx
+      const idx = this.comment.cr_idx
       this.$confirm(
         {
           message: `삭제하시겠습니까?`,
@@ -79,7 +79,7 @@ export default {
           if(Number(reaponse.data.u_isAdmin)){
               this.isAuth = true
           }else{
-              if (this.comment.r_name === currentUser){
+              if (this.comment.cr_name === currentUser){
                   this.isAuth = true
               }
           }
