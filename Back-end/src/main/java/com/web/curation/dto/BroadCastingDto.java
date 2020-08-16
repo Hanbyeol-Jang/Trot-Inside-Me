f985ce1ca9e672f274c7b512f4ae882f13123554
임지민 <lmjm1998@naver.com>
2020-08-16 15:09:41 +0900

    편성표 고침

--- Back-end/src/main/java/com/web/curation/dto/BroadCastingDto.java
+++ Back-end/src/main/java/com/web/curation/dto/BroadCastingDto.java
@@ -5,10 +5,11 @@ import lombok.Data;
 @Data
 public class BroadCastingDto {
 	private int bc_idx;
+	private int a_idx;
+	private int s_idx;
 	private String bc_company;
 	private String bc_date;
 	private String bc_time;
-	private String bc_member;
 	private String bc_title;
-	private int a_idx;
+	private String bc_member;
 }
