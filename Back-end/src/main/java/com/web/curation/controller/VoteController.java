0a916f3f8e5ca4d48933320871a25cf441028b67
Hanbyeol-Jang <35832112+Hanbyeol-Jang@users.noreply.github.com>
2020-08-17 14:28:07 +0900

    투표 비율 추가

--- Back-end/src/main/java/com/web/curation/controller/VoteController.java
+++ Back-end/src/main/java/com/web/curation/controller/VoteController.java
@@ -73,6 +73,9 @@ public class VoteController {
 			int lastWeek = voteService.lastWeek(s_name);
 			int lastWeekTotal = voteService.lastWeekTotal(s_name);
 
+			double thisWeekRatio = Math.round(((double) thisWeek / (double) thisWeekTotal * 1000)) / 100.0;
+			double lastWeekRatio = Math.round(((double) lastWeek / (double) lastWeekTotal * 1000)) / 100.0;
+
 			int thisWeekRank = voteService.thisWeekRank(s_idx);
 			int lastWeekRank = voteService.lastWeekRank(s_name);
 
@@ -83,6 +86,9 @@ public class VoteController {
 			map.put("thisWeekTotal", thisWeekTotal);
 			map.put("lastWeek", lastWeek);
 			map.put("lastWeekTotal", lastWeekTotal);
+
+			map.put("thisWeekRatio", thisWeekRatio);
+			map.put("lastWeekRatio", lastWeekRatio);
 
 			map.put("thisWeekRank", thisWeekRank);
 			map.put("lastWeekRank", lastWeekRank);
